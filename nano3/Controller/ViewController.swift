39235f585dfad4f5e7ea0614afa34dceb990d48b
Rodrigo Longhi Guimarães <rguimaraes@alunos.utfpr.edu.br>
2018-02-07 20:25:41 -0200

    Tableview now shows various games

--- nano3/Controller/ViewController.swift
+++ nano3/Controller/ViewController.swift
@@ -7,12 +7,18 @@
 //
 
 import UIKit
+import ObjectMapper
 
-class ViewController: UIViewController, ServiceDelegate {
+class ViewController: UIViewController, ServiceDelegate, UITableViewDataSource, UITableViewDelegate {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    var gameList = [Game]()
     
     func didReceiveResponse(status: StatusCode, responseJSON: String?) {
         if status == .Success {
-            print(responseJSON!)
+            gameList = Mapper<Game>().mapArray(JSONString: responseJSON!)!
+            tableView.reloadData()
         } else {
             //TODO: Alert!
         }
@@ -22,6 +28,31 @@ class ViewController: UIViewController, ServiceDelegate {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         Services.shared.getAllGamesFromAYear(delegateTarget: self)
+        tableView.dataSource = self
+        tableView.delegate = self
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return gameList.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "gameCell", for: indexPath)
+        
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 150
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
     }
 
     override func didReceiveMemoryWarning() {
